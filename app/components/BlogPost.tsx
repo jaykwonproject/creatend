4d2d2e7872e9125f38633158fd5adecdca341beb
jaykwonproject <jaykwonproject@gmail.com>
2023-12-19 14:16:32 -0500

    created components and used them in template pages

---
+++ app/components/BlogPost.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlogPost = () => {
+  return (
+    <>
+    <div>post</div>
+    <p>This is a Post component</p>
+    </>
+    
+  )
+}
+
+export default BlogPost
