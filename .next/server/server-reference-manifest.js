4eaa3bd5acc27a36126f1089d3f25c1b441c30ae
jaykwonproject <jaykwonproject@gmail.com>
2023-12-19 10:52:44 -0500

    added daisyui and updated gitignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JfsIvnszY0fIZIHSBx8hHII5myq45m7LGuHzvtXwnDE=\"\n}"
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           Creatend Main Page
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            By HyukJoo Kwon
+            <button className='btn btn-primary'>By HyukJoo Kwon</button>
         </div>
       </div>
 
